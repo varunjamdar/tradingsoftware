afd33cf442b6f3a094cce7c8f35cf55f7b57b65f
joybanerjee <joybanerjee@ee0b6ebc-50b2-4add-a1aa-35737a7b81d9>
2011-02-21 18:43:23 +0000

    customer and supplier details by joy .

---
+++ tradingSoftware/tradingSoftware/cust supplier details.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace tradingSoftware
+{
+    /// <summary>
+    /// Interaction logic for cust_supplier_details.xaml
+    /// </summary>
+    public partial class cust_supplier_details : Page
+    {
+        public cust_supplier_details()
+        {
+            InitializeComponent();
+        }
+    }
+}
