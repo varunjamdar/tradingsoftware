f8dac0b5800fe22a5fc4fd594785e173c7528784
Janice <Janice@ee0b6ebc-50b2-4add-a1aa-35737a7b81d9>
2011-02-26 11:05:22 +0000

    Journal Setup added..!!

---
+++ trunk/tradingSoftware/tradingSoftware/JournalSetup.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace tradingSoftware
+{
+    /// <summary>
+    /// Interaction logic for Journal.xaml
+    /// </summary>
+    public partial class Journal : Window
+    {
+        public Journal()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox13_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
