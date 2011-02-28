63e46256bd80ada4d8e6acc2ef4ce568cffdc566
vhjamdar <vhjamdar@ee0b6ebc-50b2-4add-a1aa-35737a7b81d9>
2011-02-28 20:40:08 +0000

    Transaction->Account relation done !!

--- trunk/tradingSoftware/tradingSoftware/JournalSetup.xaml.cs
+++ trunk/tradingSoftware/tradingSoftware/JournalSetup.xaml.cs
@@ -10,6 +10,10 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+
 
 namespace tradingSoftware
 {
@@ -21,6 +25,21 @@ namespace tradingSoftware
         public Journal()
         {
             InitializeComponent();
+
+            SqlConnection con = new SqlConnection();
+            SqlCommand cmd = new SqlCommand();
+            SqlDataAdapter adpt = new SqlDataAdapter();
+            System.Data.DataSet ds = new System.Data.DataSet();
+
+            con.ConnectionString = tradingSoftware.Properties.Settings.Default.TradeConnectionString;
+            cmd.Connection = con;
+            cmd.CommandType = System.Data.CommandType.Text;
+            cmd.CommandText = "SELECT t.Date, a.AccountName, b.AccountName AS Expr1, t.Amount FROM Transactions AS t INNER JOIN Account AS a ON t.ByAccountID = a.AccountID INNER JOIN Account AS b ON t.ToAccountID = b.AccountID";
+            con.Open();
+            adpt.SelectCommand = cmd;
+
+            adpt.Fill(ds);
+            //dataGrid2.ItemsSource = ds.Tables[0].DefaultView; //You can directly set the datagrid Itemssource; or you can create a IDictionary<string, Transaction> create transaction objects based on the dataset rows and then set the datagrid data..
         }
 
         private void textBox13_TextChanged(object sender, TextChangedEventArgs e)
