84909dc7a463c16f71fa4ee1c49e81adf9946fd5
vhjamdar <vhjamdar@ee0b6ebc-50b2-4add-a1aa-35737a7b81d9>
2011-03-04 08:43:51 +0000

    merged location setup by shailesh..

---
+++ tradingSoftware/tradingSoftware/Location.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace tradingSoftware
+{
+    /// <summary>
+    /// Interaction logic for Location.xaml
+    /// </summary>
+    public partial class Location : Window
+    {
+        DataLogic dl;
+        string state, city;
+        public Location()
+        {
+            InitializeComponent();
+            dl = new DataLogic();
+            LocationSetupGrid.DataContext = dl.showState();
+            comboBoxState.Text = "--add State--";
+            txtCity.Text = "--add City--";
+           //
+            fillStateCityListBox();
+        }
+        public void fillStateCityListBox()
+        {
+            Database.TradeDataSet ds = new Database.TradeDataSet();
+            Database.TradeDataSetTableAdapters.StateTableAdapter stateAdpt = new Database.TradeDataSetTableAdapters.StateTableAdapter();
+            Database.TradeDataSetTableAdapters.CityTableAdapter cityAdpt = new Database.TradeDataSetTableAdapters.CityTableAdapter();
+            stateAdpt.Fill(ds.State);
+            cityAdpt.Fill(ds.City);
+            EditGrid.DataContext = ds.State;
+        }
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            state = comboBoxState.Text;
+            city = txtCity.Text;
+            string Msg= dl.addStateCity(state,txtCity.Text);
+            
+                MessageBox.Show(Msg);
+
+                comboBoxState.Text = "--add State--";
+                txtCity.Text = "--add City--";
+
+            //refresh the state city list Box and textBoxes
+                fillStateCityListBox();
+
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnEdit_Click_1(object sender, RoutedEventArgs e)
+        {
+            string StateID, CityID;
+            StateID=labelStateID.Content.ToString();
+            try
+            {
+                CityID = labelCityID.Content.ToString();
+            }
+            catch (NullReferenceException nre)
+            {
+                CityID = "";
+            }
+            
+            string msg = dl.updateStateCity(StateID, textBoxState.Text, CityID, textBoxCity.Text);
+            MessageBox.Show(msg);
+            fillStateCityListBox();
+        }
+
+        private void btnDeleteCity_Click_1(object sender, RoutedEventArgs e)
+        {
+            //delete city
+            dl.deleteCity(Int32.Parse(labelCityID.Content.ToString()));
+            //refresh the state city list Box and textBoxes
+            fillStateCityListBox();
+
+
+        }
+
+        private void btnDeleteState_Click(object sender, RoutedEventArgs e)
+        {
+            //delete state
+            dl.deleteState(Int32.Parse(labelStateID.Content.ToString()));
+            //refresh the state city list Box and textBoxes
+            fillStateCityListBox();
+
+        }
+
+        private void textBoxCity_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (textBoxCity.Text == "")
+            {
+                btnDeleteCity.IsEnabled = false;
+            }
+            else
+            {
+                btnDeleteCity.IsEnabled = true;
+            }
+        }
+
+        
+
+    }
+}
