803697b21419fdc20c2300b714903d576f370a50
vhjamdar <vhjamdar@ee0b6ebc-50b2-4add-a1aa-35737a7b81d9>
2011-03-04 18:47:41 +0000

    started Trial Balance.

---
+++ trunk/tradingSoftware/tradingSoftware/TrialBalance.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace tradingSoftware
+{
+    /// <summary>
+    /// Interaction logic for Trial_Balance.xaml
+    /// </summary>
+    public partial class TrialBalance : Window
+    {
+        public TrialBalance()
+        {
+            InitializeComponent();
+        }
+    }
+}
