f15e061491327f3c8988c6b4a525c75c01a5df96
Janice <Janice@ee0b6ebc-50b2-4add-a1aa-35737a7b81d9>
2011-03-23 13:16:08 +0000

    Add unit committed

---
+++ tradingSoftware/tradingSoftware/UnitObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace tradingSoftware
+{
+    class UnitObject
+    {
+        public int UnitId { get; set; }
+        public String UnitName { get; set; }
+        public String UnitPrintName { get; set; }
+    }
+}
