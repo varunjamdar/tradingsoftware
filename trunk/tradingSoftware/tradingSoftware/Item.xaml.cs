2a4796869af3d4492868756f2f3dddfee6ab1fa0
Janice <Janice@ee0b6ebc-50b2-4add-a1aa-35737a7b81d9>
2011-03-28 20:14:24 +0000

    item is too long..yet to complete

--- trunk/tradingSoftware/tradingSoftware/Item.xaml.cs
+++ trunk/tradingSoftware/tradingSoftware/Item.xaml.cs
@@ -21,6 +21,47 @@ namespace tradingSoftware
         public Item()
         {
             InitializeComponent();
+
+            TradeDataSet tds = new TradeDataSet();
+
+            TradeDataSetTableAdapters.ItemGroupTableAdapter itemGrpTableAdpt = new tradingSoftware.TradeDataSetTableAdapters.ItemGroupTableAdapter();
+            itemGrpTableAdpt.Fill(tds.ItemGroup);
+
+            TradeDataSetTableAdapters.UnitTableAdapter unitTableAdpt = new tradingSoftware.TradeDataSetTableAdapters.UnitTableAdapter();
+            unitTableAdpt.Fill(tds.Unit);
+
+            this.cbxItemGroup.DataContext = tds.ItemGroup;
+            this.cbxUnitUsed.DataContext = tds.Unit;
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            //checking only for item code, item name, item group and unit used..
+
+            if (txtItemCode.Text == "")
+            {
+                MessageBox.Show("Enter the Item Code, cant proceed further", "Warning");
+                return;
+            }
+            if (txtName.Text == "")
+            {
+                MessageBox.Show("Enter the Name of the Item to be configured, cant proceed further", "Warning");
+                return;
+            }
+
+            if (cbxItemGroup.SelectedItem == null)
+            {
+                MessageBox.Show("Please select the group under which this item is", "Warning");
+                return;
+            }
+
+            if (cbxUnitUsed.SelectedItem == null)
+            {
+                MessageBox.Show("Please select the unit of this item", "Warning");
+                return;
+            }
+
+            //to store unid id and item group id used .selectedvalue
         }
     }
 }
