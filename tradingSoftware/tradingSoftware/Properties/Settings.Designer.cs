7195ebed93aa6759015e14f261fcf352a1f81a3d
vhjamdar <vhjamdar@ee0b6ebc-50b2-4add-a1aa-35737a7b81d9>
2011-04-06 19:37:08 +0000

    changes in sales form

--- tradingSoftware/tradingSoftware/Properties/Settings.Designer.cs
+++ tradingSoftware/tradingSoftware/Properties/Settings.Designer.cs
@@ -26,18 +26,6 @@ namespace tradingSoftware.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=.\\SQLEXPRESS;AttachDbFilename=\"C:\\Program Files\\Microsoft SQL Server\\" +
-            "MSSQL.1\\MSSQL\\Data\\Trade.mdf\";Integrated Security=True;Connect Timeout=30;User I" +
-            "nstance=True")]
-        public string TradeConnectionString {
-            get {
-                return ((string)(this["TradeConnectionString"]));
-            }
-        }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
         [global::System.Configuration.DefaultSettingValueAttribute("Data Source=.\\SQLEXPRESS;AttachDbFilename=|DataDirectory|\\Database\\Trade.mdf;Inte" +
             "grated Security=True;Connect Timeout=30;User Instance=True")]
         public string EarlierConnectionString {
@@ -67,6 +55,18 @@ namespace tradingSoftware.Properties {
         public string VJConStr {
             get {
                 return ((string)(this["VJConStr"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=.\\SQLEXPRESS;AttachDbFilename=\"C:\\Program Files\\Microsoft SQL Server\\" +
+            "MSSQL.1\\MSSQL\\Data\\Trade.mdf\";Integrated Security=True;Connect Timeout=30;User I" +
+            "nstance=True")]
+        public string TradeConnectionString {
+            get {
+                return ((string)(this["TradeConnectionString"]));
             }
         }
     }
