97672f85648d432885fedab5e2d321cf601980d8
kalsariyashailesh <kalsariyashailesh@ee0b6ebc-50b2-4add-a1aa-35737a7b81d9>
2011-04-07 05:59:47 +0000

    Purchase Order Form Added and completed. Working on Purchase1(Goods receipt Note)..(Editing) ha ha ha

--- trunk/tradingSoftware/tradingSoftware/PurchaseOrder.xaml.cs
+++ trunk/tradingSoftware/tradingSoftware/PurchaseOrder.xaml.cs
@@ -14,24 +14,18 @@ using System.Windows.Shapes;
 namespace tradingSoftware
 {
     /// <summary>
-    /// Interaction logic for Purchase1.xaml
+    /// Interaction logic for PurchaseOrder.xaml
     /// </summary>
-    public partial class Purchase1 : Window
+    public partial class PurchaseOrder : Window
     {
         DataLogic dl = new DataLogic();
-        public Purchase1()
+        public PurchaseOrder()
         {
             InitializeComponent();
             dtPick_PODate.Text = DateTime.Today.Date.ToShortDateString();
             txt_PurchaseNo.Focus();
 
-            //-------tax detail--
-
             TradeDataSet ds = new TradeDataSet();
-            TradeDataSetTableAdapters.TaxTableAdapter taxApdt = new tradingSoftware.TradeDataSetTableAdapters.TaxTableAdapter();
-            taxApdt.Fill(ds.Tax);
-            TaxDetailsGrid.DataContext = ds.Tax;
-
 
             //---Supplier
             TradeDataSetTableAdapters.SupplierTableAdapter supplierApdt = new tradingSoftware.TradeDataSetTableAdapters.SupplierTableAdapter();
@@ -47,26 +41,103 @@ namespace tradingSoftware
             itemAdpt.Fill(ds.Item);
 
             //--Max Purchase Id
-            txt_PurchaseNo.Text= dl.getPurchaseOrderNo().ToString();
+            txt_PurchaseNo.Text = dl.getPurchaseOrderNo().ToString();
         }
+
 
         private void btn_AddPurchaseItem_Click(object sender, RoutedEventArgs e)
         {
+
+            //Validation
+            bool Boolerror = false;
+            Dictionary<int, string> error = new Dictionary<int, string>();
+            int count = 0;
+
+            //Date Validation
+            try
+            {
+                DateTime dt = DateTime.Parse(dtPick_PODate.Text);
+            }
+            catch (FormatException)
+            {
+                error[count++] = "Invalid Date";
+                Boolerror = true;
+            }
+
+            //
+            if (cb_Supplier.Text == "")
+            {
+                error[count++]="Select 'Supplier'";
+                Boolerror = true;
+            }
+            else if (cb_ItemGroup.Text == "")
+            {
+                error[count++]="Select 'Item Group'";
+                Boolerror = true;
+            }
+            if (cb_Item.Text == "")
+            {
+                error[count++]="Select 'Item'";
+                Boolerror = true;
+            }
+
+            try
+            {
+                int q = Int32.Parse(txt_Quantity.Text);
+                if (q <= 0)
+                {
+                    error[count++] = "Quantity must be greater than 0";
+                    Boolerror = true;
+                }
+                
+            }
+            catch (FormatException)
+            {
+                error[count++] = "Invalid 'Quantity'";
+                Boolerror = true;
+            }
+
+            
+
+            try
+            {
+                float p = float.Parse(txt_PPU.Text);
+            }
+            catch (FormatException)
+            {
+                error[count++]="Invalid 'Price'";
+                Boolerror = true;
+            }
+
+
+            if (Boolerror == true)
+            {
+                string errorMsg="";
+                for(int i=0;i<error.Count;i++)
+                {
+                    errorMsg+=(i+1) +". "+ error[i]+"\n";
+                }
+
+                MessageBox.Show(@errorMsg,"Error !",MessageBoxButton.OK,MessageBoxImage.Error);
+                return;
+            }
+            //
             int IPONO = Int32.Parse(txt_PurchaseNo.Text);
             int IQuantity = Int32.Parse(txt_Quantity.Text);
             float FPPU = float.Parse(txt_PPU.Text);
-            addRow(cb_ItemGroup.Text,cb_Item.Text,IQuantity,FPPU);
+            addRow(cb_ItemGroup.Text, cb_Item.Text, IQuantity, FPPU);
 
 
             //refresh the Both Amount Label
             lblTotalAmount.Content = getTotalAmountOFListViewPurchaseOrder();
-            lblItemTaxAmount.Content = getTotalAmountOFListViewPurchaseOrder() + getTotalAmountTax();
+
+            //lblItemTaxAmount.Content = getTotalAmountOFListViewPurchaseOrder() + getTotalAmountTax();
         }
 
-        //get Total Amount
+        //get Total Amount of Only ListView
         public float getTotalAmountOFListViewPurchaseOrder()
         {
-            float tAmount=0;
+            float tAmount = 0;
             for (int i = 0; i <= listViewPurchseOrder.Items.Count - 1; i++)
             {
                 ListViewPurchaseOrder lvc = (ListViewPurchaseOrder)listViewPurchseOrder.Items[i];
@@ -78,12 +149,12 @@ namespace tradingSoftware
 
         private void RefreshListView(int v1, string v2, string v3, string v4, string v5, int v6, float v7)
         {
-           
+
         }
 
-        public void addRow(string v4,string v5,int v6,float v7)
+        public void addRow(string v4, string v5, int v6, float v7)
         {
-            listViewPurchseOrder.Items.Add(new ListViewPurchaseOrder(v4,v5,v6,v7));
+            listViewPurchseOrder.Items.Add(new ListViewPurchaseOrder(v4, v5, v6, v7));
             dtPick_PODate.Text = DateTime.Today.Date.ToShortDateString();
             cb_ItemGroup.Text = "";
             cb_Item.Text = "";
@@ -100,11 +171,11 @@ namespace tradingSoftware
                 cb_ItemGroup.Text = lvpo.ItemGroup;
                 cb_Item.Text = lvpo.Item1;
                 txt_Quantity.Text = lvpo.Quantity.ToString();
-         
+
                 txt_PPU.Text = lvpo.PricePerUnit.ToString();
             }
             catch (NullReferenceException nre) { }
-            
+
         }
 
         private void btn_Close_Click(object sender, RoutedEventArgs e)
@@ -124,7 +195,8 @@ namespace tradingSoftware
 
             //refresh the Both Amount Label
             lblTotalAmount.Content = getTotalAmountOFListViewPurchaseOrder();
-            lblItemTaxAmount.Content = getTotalAmountOFListViewPurchaseOrder() + getTotalAmountTax();
+
+            //lblItemTaxAmount.Content = getTotalAmountOFListViewPurchaseOrder() + getTotalAmountTax();
 
         }
 
@@ -164,7 +236,8 @@ namespace tradingSoftware
 
             //refresh the Both Amount Label
             lblTotalAmount.Content = getTotalAmountOFListViewPurchaseOrder();
-            lblItemTaxAmount.Content = getTotalAmountOFListViewPurchaseOrder() + getTotalAmountTax();
+
+            //lblItemTaxAmount.Content = getTotalAmountOFListViewPurchaseOrder() + getTotalAmountTax();
         }
 
         private void btn_RemoveItem_Click(object sender, RoutedEventArgs e)
@@ -179,162 +252,53 @@ namespace tradingSoftware
 
             //refresh the Both Amount Label
             lblTotalAmount.Content = getTotalAmountOFListViewPurchaseOrder();
-            lblItemTaxAmount.Content = getTotalAmountOFListViewPurchaseOrder() + getTotalAmountTax();
-        }
-//------------------Tax Tab----------------------------------------------------
-        private void btnCloseTax_Click(object sender, RoutedEventArgs e)
-        {
-            this.Close();
+
+            //lblItemTaxAmount.Content = getTotalAmountOFListViewPurchaseOrder() + getTotalAmountTax();
         }
 
-        private void btnClearTax_Click(object sender, RoutedEventArgs e)
-        {
-            cb_TaxName.Text = "";
-            //cb_ValuePer.SelectedIndex = 0;
-            txtAmount.Text = "";
-            cb_Type.SelectedIndex = 0;
-        }
-
-        private void listViewTaxDetails_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
-
-        private void btnResetAllTax_Click(object sender, RoutedEventArgs e)
-        {
-            cb_TaxName.Text = "";
-            rbPercentage.IsChecked = true;
-            rbValue.IsChecked = false;
-            txtAmount.Text = "";
-            cb_Type.SelectedIndex = 0;
-            listViewTaxDetails.Items.Clear();
-
-            //Displat Total Amount Tax
-            lblTotalAmountTax.Content = getTotalAmountTax();
-            lblItemTaxAmount.Content = getTotalAmountOFListViewPurchaseOrder() + getTotalAmountTax();
-        }
-
-        private void rbPercentage_Checked(object sender, RoutedEventArgs e)
-        {
-           // txtPercentage.Visibility;
-           // txtAmount.Visibility = Visibility.Hidden;
-        }
-
-        private void rbValue_Checked(object sender, RoutedEventArgs e)
-        {
-            //txtPercentage.Visibility = Visibility.Hidden;
-            //txtAmount.Visibility = Visibility.Visible;
-        }
-
-        public float getTotalAmountTax()
-        {
-            float tAmountTax = 0;
-            for (int i = 0; i <= listViewTaxDetails.Items.Count - 1; i++)
-            {
-                ListViewPurchaseTaxDetails lvc = (ListViewPurchaseTaxDetails)listViewTaxDetails.Items[i];
-                if (lvc.TaxType == "Exclusive")
-                {
-                    tAmountTax += lvc.TaxAmount;
-                }
-            }
-            return tAmountTax;
-
-        }
-
-        private void btnAddTax_Click(object sender, RoutedEventArgs e)
-        {
-            float p=0, a=0;
-            if(rbPercentage.IsChecked==true)
-            {
-            //    MessageBox.Show(lblPercentageTax.Content.ToString());
-                p = float.Parse( lblPercentageTax.Content.ToString());//txtPercentage.Text;
-            }
-            else
-            {
-                if (txtAmount.Text == "")
-                {
-                    txtAmount.Text = "0";
-                }
-                a=float.Parse(txtAmount.Text);
-            }
-            listViewTaxDetails.Items.Add(new ListViewPurchaseTaxDetails(cb_TaxName.Text,p,a,cb_Type.Text,float.Parse(lblTotalAmount.Content.ToString())));
-            cb_TaxName.Text = "";
-            rbPercentage.IsChecked = true;
-            rbValue.IsChecked = false;
-            txtAmount.Text = "";
-            cb_Type.SelectedIndex = 0;
-            listViewTaxDetails.SelectedIndex = -1;
-
-            cb_Type.SelectedIndex = -1;
-
-            //Displat Total Amount Tax
-            lblTotalAmountTax.Content = getTotalAmountTax();
-            lblItemTaxAmount.Content = getTotalAmountOFListViewPurchaseOrder() + getTotalAmountTax();
-        }
-
-        private void btnRemoveTax_Click(object sender, RoutedEventArgs e)
-        {
-            listViewTaxDetails.Items.Remove(listViewTaxDetails.SelectedItem);
-            cb_TaxName.Text = "";
-            rbPercentage.IsChecked = true;
-            rbValue.IsChecked = false;
-            txtAmount.Text = "";
-            listViewTaxDetails.SelectedIndex = -1;
-
-            //Displat Total Amount Tax
-            lblTotalAmountTax.Content = getTotalAmountTax();
-            lblItemTaxAmount.Content = getTotalAmountOFListViewPurchaseOrder() + getTotalAmountTax();
-        }
-
-        private void cb_Type_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            //if (cb_Type.Text == "Inclusive")
-            //{
-                
-
-            //    rbPercentage.Visibility = Visibility.Visible;
-            //    rbValue.Visibility = Visibility.Visible;
-            //    lblPercentageTax.Visibility = Visibility.Visible;
-            //    txtAmount.Visibility = Visibility.Visible;
-            //}
-
-            //if (cb_Type.Text == "Exclusive")
-            //{
-            //    rbPercentage.Visibility = Visibility.Hidden;
-            //    rbValue.Visibility = Visibility.Hidden;
-            //    lblPercentageTax.Visibility = Visibility.Hidden;
-            //    txtAmount.Visibility = Visibility.Hidden;
-            //}
-        }
+        //Place purchase order
 
         private void btnPlacePurchaseOrder_Click(object sender, RoutedEventArgs e)
         {
-            MessageBoxResult mbr = MessageBox.Show("Are You Sure to place purchase Order ?", "Varifiacation", MessageBoxButton.YesNo,MessageBoxImage.Question);
+            
+            if (listViewPurchseOrder.Items.Count == 0)
+            {
+                MessageBox.Show("Please Add Item", "Error !", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            MessageBoxResult mbr = MessageBox.Show("Are You Sure to place purchase Order ?", "Varifiacation", MessageBoxButton.YesNo, MessageBoxImage.Question);
 
             if (mbr == MessageBoxResult.Yes)
             {
                 string supplierName = (string)cb_Supplier.Text;
-                
+
                 int supplierId = dl.getSupplierId(supplierName);
-                dl.placePurchaseOrder_PurchaseOrderTable(Int32.Parse(txt_PurchaseNo.Text), DateTime.Parse(dtPick_PODate.Text),supplierId, float.Parse(lblTotalAmount.Content.ToString()));
+                dl.placePurchaseOrder_PurchaseOrderTable(Int32.Parse(txt_PurchaseNo.Text), DateTime.Parse(dtPick_PODate.Text), supplierId, float.Parse(lblTotalAmount.Content.ToString()));
 
 
                 //For Each and Every Item place to the PurchaseOrderItems table
                 for (int i = 0; i <= listViewPurchseOrder.Items.Count - 1; i++)
                 {
                     ListViewPurchaseOrder lvc = (ListViewPurchaseOrder)listViewPurchseOrder.Items[i];
-                    dl.placePurchaseOrder_PurchaseOrderItemsTable(Int32.Parse(txt_PurchaseNo.Text),dl.getItemId(lvc.Item1), lvc.Quantity, lvc.PricePerUnit);
+                    dl.placePurchaseOrder_PurchaseOrderItemsTable(Int32.Parse(txt_PurchaseNo.Text), dl.getItemId(lvc.Item1), lvc.Quantity, lvc.PricePerUnit);
                 }
 
-
+                /*
                 //For Each and Every Item Tax to the PurchaseOrderTaxes table
                 for (int i = 0; i <= listViewTaxDetails.Items.Count - 1; i++)
                 {
                     ListViewPurchaseTaxDetails lvc = (ListViewPurchaseTaxDetails)listViewTaxDetails.Items[i];
                     dl.placePurchaseOrder_PurchaseOrderTaxesTable(Int32.Parse(txt_PurchaseNo.Text), dl.getTaxId(lvc.TaxName), lvc.TaxType, lvc.TaxAmount);
                 }
-
-                MessageBox.Show("Order Placed Successfully","Succeeded",MessageBoxButton.OK,MessageBoxImage.Information);
+                */
+                MessageBox.Show("Order Placed Successfully", "Succeeded", MessageBoxButton.OK, MessageBoxImage.Information);
+                //--Max Purchase Id
+                //Remaining. cant get max id, reset all
+                //MessageBox.Show(dl.getPurchaseOrderNo().ToString());
+                //txt_PurchaseNo.Text = dl.getPurchaseOrderNo().ToString();
+                //btn_ResetItems_Click();
+                //InitializeComponent();
             }
         }
     }
