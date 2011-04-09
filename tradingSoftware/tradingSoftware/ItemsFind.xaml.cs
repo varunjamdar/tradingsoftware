4ef60ec41bf56a0b04992a22d02fe0f977c42406
vhjamdar <vhjamdar@ee0b6ebc-50b2-4add-a1aa-35737a7b81d9>
2011-04-09 15:15:52 +0000

    Solved the datagrid.SelectedRow issue. Mind It.

--- tradingSoftware/tradingSoftware/ItemsFind.xaml.cs
+++ tradingSoftware/tradingSoftware/ItemsFind.xaml.cs
@@ -11,6 +11,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
+using System.Collections;
+using System.Data;
+using Microsoft.Windows.Controls.Primitives;
+using Microsoft.Windows.Controls;
+
 namespace tradingSoftware
 {
     /// <summary>
@@ -19,6 +24,7 @@ namespace tradingSoftware
     public partial class ItemsFind : Window
     {
         private DataLogic dl = null;
+        private int itemCode;
 
         public ItemsFind()
         {
@@ -39,12 +45,68 @@ namespace tradingSoftware
 
         private void btnSelect_Click(object sender, RoutedEventArgs e)
         {
-            
+
         }
 
         private void btnCancel_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        private void ItemGrid_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                Helper helperClass = new Helper();
+                helperClass.HelperDataGrid = this.ItemGrid;
+
+                DependencyObject dep = (DependencyObject)e.OriginalSource;
+
+                while ((dep != null) && !(dep is DataGridCell) && !(dep is DataGridColumnHeader))
+                {
+                    dep = VisualTreeHelper.GetParent(dep);
+                }
+
+                if (dep == null)
+                    return;
+    
+                if (dep is DataGridColumnHeader)
+                {
+                    return;
+                }
+    
+                if (dep is DataGridCell)
+                {
+                    DataGridCell cell = dep as DataGridCell;
+
+                    // navigate further up the tree
+                    while ((dep != null) && !(dep is DataGridRow))
+                    {
+                        dep = VisualTreeHelper.GetParent(dep);
+                    }
+
+                    if (dep == null)
+                        return;
+
+                    DataGridRow row = dep as DataGridRow;
+
+                    //object value = ExtractBoundValue(row, cell);
+
+                    //int columnIndex = cell.Column.DisplayIndex;
+                    int rowIndex = helperClass.FindRowIndex(row);
+
+                    //ClickedItemDisplay.Text = string.Format("Cell clicked [{0}, {1}] = {2}", rowIndex, columnIndex, value.ToString());
+
+                    DataGridCell dgc = helperClass.GetCell(rowIndex, 0);
+                    object valueToShow = helperClass.ExtractBoundValue(row, dgc);
+                    MessageBox.Show(valueToShow.ToString());
+                    itemCode = int.Parse(valueToShow.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                
+            }
         }
     }
 }
