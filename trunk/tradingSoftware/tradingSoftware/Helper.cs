d7ce167e480a000014ace06526be30f1cf2256ad
vhjamdar <vhjamdar@ee0b6ebc-50b2-4add-a1aa-35737a7b81d9>
2011-04-09 15:15:52 +0000

    Solved the datagrid.SelectedRow issue. Mind It.

---
+++ trunk/tradingSoftware/tradingSoftware/Helper.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Windows.Controls;
+using Microsoft.Windows.Controls.Primitives;
+using System.ComponentModel;
+
+namespace tradingSoftware
+{
+    public class Helper
+    {
+        public DataGrid HelperDataGrid { get; set; }
+
+        /// <summary>
+        /// Determine the index of a DataGridRow
+        /// </summary>
+        /// <param name="row"></param>
+        /// <returns></returns>
+        public int FindRowIndex(DataGridRow row)
+        {
+            DataGrid dataGrid = ItemsControl.ItemsControlFromItemContainer(row) as DataGrid;
+
+            int index = dataGrid.ItemContainerGenerator.IndexFromContainer(row);
+
+            return index;
+        }
+
+        /// <summary>
+        /// Find the value that is bound to a DataGridCell
+        /// </summary>
+        /// <param name="row"></param>
+        /// <param name="cell"></param>
+        /// <returns></returns>
+        public object ExtractBoundValue(DataGridRow row, DataGridCell cell)
+        {
+            // find the property that this cell's column is bound to
+            string boundPropertyName = FindBoundProperty(cell.Column);
+
+            // find the object that is realted to this row
+            object data = row.Item;
+
+            // extract the property value
+            PropertyDescriptorCollection properties = TypeDescriptor.GetProperties(data);
+            PropertyDescriptor property = properties[boundPropertyName];
+            object value = property.GetValue(data);
+
+            return value;
+        }
+
+        /// <summary>
+        /// Find the name of the property which is bound to the given column
+        /// </summary>
+        /// <param name="col"></param>
+        /// <returns></returns>
+        public string FindBoundProperty(DataGridColumn col)
+        {
+            DataGridBoundColumn boundColumn = col as DataGridBoundColumn;
+
+            // find the property that this column is bound to
+            Binding binding = boundColumn.Binding as Binding;
+            string boundPropertyName = binding.Path.Path;
+
+            return boundPropertyName;
+        }
+
+        public DataGridCell GetCell(int row, int column)
+        {
+            DataGridRow rowContainer = GetRow(row);
+
+            if (rowContainer != null)
+            {
+                DataGridCellsPresenter presenter = GetVisualChild<DataGridCellsPresenter>(rowContainer);
+
+                // try to get the cell but it may possibly be virtualized
+                DataGridCell cell = (DataGridCell)presenter.ItemContainerGenerator.ContainerFromIndex(column);
+                if (cell == null)
+                {
+                    // now try to bring into view and retreive the cell
+                    this.HelperDataGrid.ScrollIntoView(rowContainer, this.HelperDataGrid.Columns[column]);
+                    cell = (DataGridCell)presenter.ItemContainerGenerator.ContainerFromIndex(column);
+                }
+                return cell;
+            }
+            return null;
+        }
+
+        public DataGridRow GetRow(int index)
+        {
+            DataGridRow row = (DataGridRow)this.HelperDataGrid.ItemContainerGenerator.ContainerFromIndex(index);
+            if (row == null)
+            {
+                // may be virtualized, bring into view and try again
+                this.HelperDataGrid.ScrollIntoView(this.HelperDataGrid.Items[index]);
+                row = (DataGridRow)this.HelperDataGrid.ItemContainerGenerator.ContainerFromIndex(index);
+            }
+            return row;
+        }
+
+        public static T GetVisualChild<T>(Visual parent) where T : Visual
+        {
+            T child = default(T);
+            int numVisuals = VisualTreeHelper.GetChildrenCount(parent);
+            for (int i = 0; i < numVisuals; i++)
+            {
+                Visual v = (Visual)VisualTreeHelper.GetChild(parent, i);
+                child = v as T;
+                if (child == null)
+                {
+                    child = GetVisualChild<T>(v);
+                }
+                if (child != null)
+                {
+                    break;
+                }
+            }
+            return child;
+        } 
+    }
+}
