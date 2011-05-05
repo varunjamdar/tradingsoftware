be1cf60f4c41df058bd254248d36e78a9a7fd6c7
vhjamdar <vhjamdar@ee0b6ebc-50b2-4add-a1aa-35737a7b81d9>
2011-05-05 20:03:31 +0000

    sales..

--- tradingSoftware/tradingSoftware/Sales.xaml.cs
+++ tradingSoftware/tradingSoftware/Sales.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
+using System.Linq.Expressions;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
@@ -16,11 +17,33 @@ namespace tradingSoftware
     /// <summary>
     /// Interaction logic for Window1.xaml
     /// </summary>
-    public partial class Window1 : Window
+    public partial class Sale : Window
     {
-        public Window1()
+        TradeDataSet ds;
+
+        public Sale()
         {
             InitializeComponent();
+
+            ds = new TradeDataSet();
+
+            TradeDataSetTableAdapters.ItemGroupTableAdapter itemGroupAdpt =
+                new tradingSoftware.TradeDataSetTableAdapters.ItemGroupTableAdapter();
+            TradeDataSetTableAdapters.ItemTableAdapter itemAdpt =
+                new tradingSoftware.TradeDataSetTableAdapters.ItemTableAdapter();
+            TradeDataSetTableAdapters.SaleTableAdapter saleAdpt =
+                new tradingSoftware.TradeDataSetTableAdapters.SaleTableAdapter();
+            TradeDataSetTableAdapters.SaleItemsTableAdapter saleItemsAdpt =
+                new tradingSoftware.TradeDataSetTableAdapters.SaleItemsTableAdapter();
+            TradeDataSetTableAdapters.SaleTaxesTableAdapter saleTaxAdpt =
+                new tradingSoftware.TradeDataSetTableAdapters.SaleTaxesTableAdapter();
+
+            itemGroupAdpt.Fill(ds.ItemGroup);
+            itemAdpt.Fill(ds.Item);
+            saleAdpt.Fill(ds.Sale);
+            saleItemsAdpt.Fill(ds.SaleItems);
+            saleTaxAdpt.Fill(ds.SaleTaxes);
+
         }
 
         private void rbPercent_Checked(object sender, RoutedEventArgs e)
@@ -29,6 +52,11 @@ namespace tradingSoftware
         }
 
         private void rbValue_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void listViewSalesItems_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
         }
---
+++ tradingSoftware/tradingSoftware/SalesClasses.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace tradingSoftware
+{
+    public class Sale
+    {
+        public List<SaleItem> SaleItems;
+        public List<SaleTax> SaleTaxes;
+        
+
+        public Sale()
+        {
+
+        }
+    }
+
+    public class SaleItem
+    {
+        public SaleItem()
+        {
+            
+        }
+
+        public SaleItem( int q, float ppu, decimal rs)
+        {
+            Quantity = q;
+            PricePerUnit = ppu;
+            this.Rs = rs;
+        }
+        public string ItemGroup { get; set; }
+        public string ItemName { get; set; }
+        public int Quantity { get; set; }
+        public float PricePerUnit { get; set; }
+        public decimal Rs { get; set; }
+    }
+
+    public class SaleTax
+    {
+        public SaleTax()
+        {
+
+        }
+    }
+}
