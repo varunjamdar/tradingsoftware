c5ce2b656d20dc417d4ae6095c51d2be6de1d32d
Janice <Janice@ee0b6ebc-50b2-4add-a1aa-35737a7b81d9>
2011-05-06 11:04:34 +0000

    customer, supplier, tax object made

---
+++ tradingSoftware/tradingSoftware/TaxObject.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace tradingSoftware
+{
+    public class TaxObject
+    {
+        private int taxId;
+        private string taxName;
+        private float percentage;
+
+        public int TaxId {
+            get
+            {
+                return taxId;
+            }
+            set
+            {
+                if (value <= 0)
+                    throw new NegativeValueException("Tax ID cannot be Negative Or 0");
+                else
+                    taxId = value;
+            }
+        }
+
+        public string TaxName {
+            get
+            {
+                return taxName;
+            }
+            set
+            {
+                if (taxName.Length <= 0)
+                    throw new NullValueException("Tax Name Cannot Be NULL");
+                else
+                    taxName = value;
+            }
+        }
+        public float Percentage {
+            get
+            {
+                return percentage;
+            }
+            set
+            {
+                percentage = value;
+            }
+        }
+    }
+}
