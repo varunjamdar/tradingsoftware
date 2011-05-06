08c00e889df6929b492a8030380efa6530d4c46e
vhjamdar <vhjamdar@ee0b6ebc-50b2-4add-a1aa-35737a7b81d9>
2011-05-06 19:04:31 +0000

    Stock View UI done !!

---
+++ trunk/tradingSoftware/tradingSoftware/StockView.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace tradingSoftware
+{
+    /// <summary>
+    /// Interaction logic for StockView.xaml
+    /// </summary>
+    public partial class StockView : Window
+    {
+        public StockView()
+        {
+            InitializeComponent();
+        }
+
+        private void btnViewStock_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
