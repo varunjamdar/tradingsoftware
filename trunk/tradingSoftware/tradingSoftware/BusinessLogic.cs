e58f9ab538e8ac58e51910494a7f2ec0d838ec34
kalsariyashailesh <kalsariyashailesh@ee0b6ebc-50b2-4add-a1aa-35737a7b81d9>
2011-05-06 10:26:25 +0000

    Deployment is also there.

---
+++ trunk/tradingSoftware/tradingSoftware/BusinessLogic.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.VisualBasic;
+
+namespace tradingSoftware
+{
+    class BusinessLogic
+    {
+        public long remainingTrialDay(string input)
+        {
+            string DateString = input+"/"; //last / for return again to if(ss=seprator) to store in YMDD array
+
+            char[] c = DateString.ToCharArray();
+
+            StringBuilder sb = new StringBuilder();
+            int[] MDYTd = new int[4];
+            int i = 0;
+            char seprator = Convert.ToChar("/");
+            foreach (char ss in c)
+            {
+
+                if (ss == seprator)
+                {
+
+                    MDYTd[i++] = Int32.Parse(sb.ToString());
+                    sb = new StringBuilder();
+                }
+                else
+                {
+                    sb.Append(ss);
+                }
+            }
+            //
+            //concept difference between date
+            DateTime dt = new DateTime(MDYTd[2], MDYTd[0],MDYTd[1]); //yy/mm/dd
+            Console.WriteLine(DateTime.Now.Date);
+            Console.WriteLine(dt.ToString());
+            long diffday = DateAndTime.DateDiff(DateInterval.Day, dt, DateTime.Now.Date, FirstDayOfWeek.Saturday, FirstWeekOfYear.FirstFullWeek);
+
+            //Calculate Remaining Trail Days
+            if (diffday < 0)
+            {//check consistancy if systemDate modified any set privios date then also Trail day will reduce 
+                //here diffday is negative(-)
+                long TrailDays = MDYTd[3] + diffday;
+                return TrailDays;
+            }
+            else
+            {
+                long TrailDays = MDYTd[3] - diffday;
+                return TrailDays;
+            }
+            
+            
+        }
+    }
+}
---
+++ trunk/tradingSoftware/tradingSoftware/EncryptionDecryption.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+using System.Security.Cryptography;
+
+namespace tradingSoftware
+{
+    class EncryptionDecryption
+    {
+        public string Encrypt(string toEncrypt)
+        {
+            byte[] toEncryptArray = UTF8Encoding.UTF8.GetBytes(toEncrypt);
+
+            //If hashing use get hashcode regards to your key
+            byte[] keyArray = { 21, 10, 64, 10, 100, 40, 200, 4,
+                    21, 54, 65, 246, 5, 62, 1, 54,
+                    54, 6, 8, 9, 65, 4, 65, 9};
+
+            TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
+            //set the secret key for the tripleDES algorithm
+            tdes.Key = keyArray;
+            //mode of operation. there are other 4 modes.
+            //We choose ECB(Electronic code Book)
+            tdes.Mode = CipherMode.ECB;
+            //padding mode(if any extra byte added)
+
+            tdes.Padding = PaddingMode.PKCS7;
+
+            ICryptoTransform cTransform = tdes.CreateEncryptor();
+            //transform the specified region of bytes array to resultArray
+            byte[] resultArray =
+              cTransform.TransformFinalBlock(toEncryptArray, 0,
+              toEncryptArray.Length);
+            //Release resources held by TripleDes Encryptor
+            tdes.Clear();
+            //Return the encrypted data into unreadable string format
+            return Convert.ToBase64String(resultArray, 0, resultArray.Length);
+        }
+
+        public string Decrypt(string cipherString)
+        {
+            
+            //get the byte code of the string
+
+            byte[] toEncryptArray = Convert.FromBase64String(cipherString);
+            
+                //if hashing was not implemented get the byte code of the key
+            byte[] keyArray = { 21, 10, 64, 10, 100, 40, 200, 4,
+                    21, 54, 65, 246, 5, 62, 1, 54,
+                    54, 6, 8, 9, 65, 4, 65, 9};//UTF8Encoding.UTF8.GetBytes(key);
+            
+            TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
+            //set the secret key for the tripleDES algorithm
+            tdes.Key = keyArray;
+            //mode of operation. there are other 4 modes. 
+            //We choose ECB(Electronic code Book)
+
+            tdes.Mode = CipherMode.ECB;
+            //padding mode(if any extra byte added)
+            tdes.Padding = PaddingMode.PKCS7;
+
+            ICryptoTransform cTransform = tdes.CreateDecryptor();
+            byte[] resultArray = cTransform.TransformFinalBlock(
+                                 toEncryptArray, 0, toEncryptArray.Length);
+            //Release resources held by TripleDes Encryptor                
+            tdes.Clear();
+            //return the Clear decrypted TEXT
+            return UTF8Encoding.UTF8.GetString(resultArray);
+        }
+    }
+}
